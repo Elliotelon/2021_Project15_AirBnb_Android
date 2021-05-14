4f535607aec64afc83fa3e19dde2323e2b8cf40e
kimminkyu <kmk50009@naver.com>
2021-05-14 13:11:41 +0900

    AirBnb 네이버 지도 API 사용

--- app/src/main/java/fastcampus/aop/part3/chapter07/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part3/chapter07/MainActivity.kt
@@ -2,10 +2,54 @@ package fastcampus.aop.part3.chapter07
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naver.maps.map.MapView
 
 class MainActivity : AppCompatActivity() {
+
+    private val mapView: MapView by lazy {
+        findViewById<MapView>(R.id.mapView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mapView.onCreate(savedInstanceState)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
